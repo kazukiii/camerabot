baa72d468b8edfe3f671872e25a8b2d0fbc96cc0
kazuki <kazumatsu.dev28@gmail.com>
2020-05-23 14:04:55 +0900

    functionalize snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -5,21 +5,26 @@ from datetime import datetime
 import requests
 from pytz import timezone
 
-subprocess.call('raspistill -o image.jpg', shell=True)
 
-now = datetime.now().astimezone(timezone('Asia/Tokyo'))
-TOKEN = os.environ.get('API_TOKEN')
-ImagePath = './image.jpg'
-initialComment = '{}:00'.format(now.strftime("%Y/%m/%d %H"))
+def main():
+    subprocess.call('raspistill -o image.jpg', shell=True)
+    now = datetime.now().astimezone(timezone('Asia/Tokyo'))
+    TOKEN = os.environ.get('API_TOKEN')
+    ImagePath = './image.jpg'
+    initialComment = '{}:00'.format(now.strftime("%Y/%m/%d %H"))
 
-CHANNEL = os.environ.get('CHANNEL')
-FILENAME = 'my_file'
-files = {'file': open(ImagePath, 'rb')}
-param = {
-    'token': TOKEN,
-    'channels': CHANNEL,
-    'filename': FILENAME,
-    'initial_comment': initialComment,
-}
+    CHANNEL = os.environ.get('CHANNEL')
+    FILENAME = 'my_file'
+    files = {'file': open(ImagePath, 'rb')}
+    param = {
+        'token': TOKEN,
+        'channels': CHANNEL,
+        'filename': FILENAME,
+        'initial_comment': initialComment,
+    }
 
-requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
+    requests.post(url="https://slack.com/api/files.upload", params=param, files=files)
+
+
+if __name__ == '__main__':
+    main()
